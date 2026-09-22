894ecdc7e632fc90510e5fb77d1527fcf0c401c9
lethephos <elinallaia@gmail.com>
2026-09-22 11:10:38 -0700

    feat: support museum artwork hydration

--- lib/museums/types.ts
+++ lib/museums/types.ts
@@ -4,6 +4,7 @@ export interface MuseumAdapter {
   id: string;
   name: string;
   search(query: string): Promise<ArtworkCandidate[]>;
+  getById?(id: string): Promise<ArtworkCandidate | null>;
 }
 
 export type Fetcher = typeof fetch;
