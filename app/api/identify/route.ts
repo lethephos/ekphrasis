f7c89df4ea2adb8109430c8c46ff8e000972b2e4
lethephos <elinallaia@gmail.com>
2026-09-22 09:47:42 -0700

    fix: pin identify route to Node.js runtime

--- app/api/identify/route.ts
+++ app/api/identify/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from "next/server";
+
+export const runtime = "nodejs";
 import { identifyImage } from "../../../lib/pipeline/identify";
 import { UpstashResultCache } from "../../../lib/cache/upstash";
 import { SlidingWindowRateLimiter, createRequestIdentity } from "../../../lib/rate-limit/rate-limit";
